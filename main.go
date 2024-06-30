bce17ef64a045987431923835fc0189fef013407
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-06-30 17:45:33 +0530

    single broadcast

--- main.go
+++ main.go
@@ -43,6 +43,56 @@ func main() {
 
 		return n.Reply(msg, body)
 	})
+
+	// Single-Node Broadcast
+	messagesNode := make(map[string][]float64)
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "broadcast_ok"
+
+		topology := n.NodeIDs()
+		for _, id := range topology {
+			_, ok := messagesNode[id]
+			if !ok {
+				messagesNode[id] = make([]float64, 0)
+			}
+			messagesNode[id] = append(messagesNode[id], body["message"].(float64))
+		}
+
+		delete(body, "message")
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "read_ok"
+		body["messages"] = messagesNode[n.ID()]
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		// Update the message type to return back.
+		body["type"] = "topology_ok"
+		delete(body, "topology")
+
+		return n.Reply(msg, body)
+	})
+
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
