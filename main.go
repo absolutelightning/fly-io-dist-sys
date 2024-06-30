b8892e6c83d58df34502e0ccf82685917bbe1ba3
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-06-30 17:08:29 +0530

    fix id generation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
+	"sync/atomic"
 	"time"
 )
 
@@ -26,9 +27,15 @@ func main() {
 	})
 
 	// Unique ID Generation
-	counter := 0
+	nodeCounterMap := make(map[string]int64)
 	n.Handle("generate", func(msg maelstrom.Message) error {
-		counter++
+		counter := int64(1)
+		val, ok := nodeCounterMap[n.ID()]
+		if ok {
+			counter = val
+		}
+		atomic.AddInt64(&counter, 1)
+		nodeCounterMap[n.ID()] = counter
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
@@ -42,10 +49,11 @@ func main() {
 			intNodeId += int(ch)
 		}
 
-		body["id"] = int64(intNodeId) + int64(counter) + time.Now().UnixNano()
+		body["id"] = int64(intNodeId) + counter + time.Now().UnixNano()
 		return n.Reply(msg, body)
 	})
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
+
 }
